bcc8a525e8df366ee487a7f662da0a9e5830925b
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-01-13 10:29:50 -0500

    Make executable separate from installation

--- Formula/ts3d.rb
+++ Formula/ts3d.rb
@@ -12,6 +12,7 @@ class Ts3d < Formula
     man_dir = "#{prefix}/man/man6"
     ts3d_root = "#{prefix}/ts3d-root"
     system 'mkdir', '-p', exe_dir, man_dir, ts3d_root
+    system 'make', 'CFLAGS="-O2"'
     system 'sh', '-c', '--', 'yes | make install exe=ts3d-real exe-dir="$0" man-dir="$1" TS3D_ROOT="$2"', exe_dir, man_dir, ts3d_root
     File.open("#{exe_dir}/ts3d", 'w') do |file|
       file.write <<~SH
