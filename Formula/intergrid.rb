21492709c8ba15f7e66ee870b7df898b5afa1db4
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-01-13 16:53:55 -0500

    Add intergrid checksum

--- Formula/intergrid.rb
+++ Formula/intergrid.rb
@@ -3,7 +3,7 @@ class Intergrid < Formula
   homepage "https://github.com/TurkeyMcMac/intergrid"
   url "https://github.com/TurkeyMcMac/intergrid/archive/1408b2e89dcbb55d2da6f860e67af25b82476560.zip"
   version "0.3.3"
-  # sha256 "???"
+  sha256 "cd765fe375555534a15410aea7f1fad6e97c74af30fc37ec214a77dc9c44db8e"
 
   depends_on "sdl2"
 
