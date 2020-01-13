cb408506c85959762dc309259d40f269d192ab16
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-01-13 10:00:01 -0500

    Get ts3d formula pretty much working

--- Formula/ts3d.rb
+++ Formula/ts3d.rb
@@ -1,12 +1,26 @@
-class TS3D < Formula
+class Ts3d < Formula
   desc "Terminal-based first-person shooter"
   homepage "https://github.com/TurkeyMcMac/ts3d"
   url "https://github.com/TurkeyMcMac/ts3d/archive/master.zip"
+  version "1.3.14"
+  sha256 "948371ecd5afa9004409e7866706edee91e82a9ff43c2997617b0705dd7c1836"
 
   depends_on "ncurses"
 
   def install
-    system 'sh', '-c', 'yes | make install exe-dir="$1" man-dir="$2"', "#{prefix}/bin/", "#{prefix}/man/man6/"
+    exe_dir = "#{prefix}/bin"
+    man_dir = "#{prefix}/man/man6"
+    ts3d_root = "#{prefix}/ts3d-root"
+    system 'mkdir', '-p', exe_dir, man_dir, ts3d_root
+    system 'sh', '-c', '--', 'yes | make install exe=ts3d-real exe-dir="$0" man-dir="$1" TS3D_ROOT="$2"', exe_dir, man_dir, ts3d_root
+    File.open("#{exe_dir}/ts3d", 'w') do |file|
+      file.write <<~SH
+        #!/bin/sh
+        export TS3D_ROOT="#{ts3d_root}"
+        #{exe_dir}/ts3d-real "$@"
+      SH
+    end
+    system 'chmod', '+x', "#{exe_dir}/ts3d"
   end
 
   def test
