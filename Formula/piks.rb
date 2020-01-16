ce87f38101b9cef77866cef7f5d4cd13afcfb190
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-01-16 08:54:24 -0500

    Upgrade piks to 0.5.7

--- Formula/piks.rb
+++ Formula/piks.rb
@@ -1,10 +1,10 @@
 class Piks < Formula
-  DOWNLOAD_COMMIT = "7406752d2b0d0731e58092f12a7c65398e165aef".freeze
+  DOWNLOAD_COMMIT = "07d2bf7c343fd6668caf1d972e0da8622fc7014a".freeze
   desc "Evolution simulator similar to Conway's Game of Life"
   homepage "https://github.com/TurkeyMcMac/piks/tree/#{DOWNLOAD_COMMIT}"
   url "https://github.com/TurkeyMcMac/piks/archive/#{DOWNLOAD_COMMIT}.tar.gz"
-  version "0.5.6"
-  sha256 "149c1266e96b6f72ccb8006db740adeba7ceed320e1706b82f5c15bfdd89d4b2"
+  version "0.5.7"
+  sha256 "6154a500c62dcaee35b90139a226711ebdd27a86edb0a26f502a6db59fd0d121"
 
   def install
     system "make", "CFLAGS=-O3 -flto"
