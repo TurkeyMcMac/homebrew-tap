db4c5fb4b3d3d19cd6503fede3d5e707ec749206
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-03-03 08:33:42 -0500

    Update intergrid to 0.3.4

--- Formula/intergrid.rb
+++ Formula/intergrid.rb
@@ -1,9 +1,9 @@
 class Intergrid < Formula
   desc "Ecological simulator/screensaver"
   homepage "https://github.com/TurkeyMcMac/intergrid"
-  url "https://github.com/TurkeyMcMac/intergrid/archive/1408b2e89dcbb55d2da6f860e67af25b82476560.tar.gz"
-  version "0.3.3"
-  sha256 "8d6cfb35597998f86449f7a2d78669f0b20a4b5a3665b3d2b5e0cb5d3760d023"
+  url "https://github.com/TurkeyMcMac/intergrid/archive/09761c2c7dd39871c8e7e5357099f2224b6bb099.tar.gz"
+  version "0.3.4"
+  sha256 "bb2e994bc67c2bb298cee3bed526d2b1eba6df4e0fddee71ab0afcd834f1f97a"
 
   depends_on "sdl2"
 
