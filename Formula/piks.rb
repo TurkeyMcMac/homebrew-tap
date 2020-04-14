55f040b2d654e5de817879e44169e485a719771e
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-04-14 10:30:54 -0400

    Update piks to 0.6.5

--- Formula/piks.rb
+++ Formula/piks.rb
@@ -1,9 +1,9 @@
 class Piks < Formula
   desc "Evolution simulator similar to Conway's Game of Life"
   homepage "https://github.com/TurkeyMcMac/piks"
-  url "https://github.com/TurkeyMcMac/piks/archive/2cb85cd3e03ab199c75a7008a0efa53619b54b57.tar.gz"
-  version "0.6.4"
-  sha256 "a2a3a51a327fc5986dc6e6b200ec0d09b8aae2c42848a80f12db7701ebccb2e4"
+  url "https://github.com/TurkeyMcMac/piks/archive/a57c5661064471ee0e743ac5e287b32ab94dd685.tar.gz"
+  version "0.6.5"
+  sha256 "be719df3807ee0808ad0cd0d63fdafbf29c901368a1aa702f02f1565306a8291"
 
   def install
     system "make", "CFLAGS=-O3 -flto"
