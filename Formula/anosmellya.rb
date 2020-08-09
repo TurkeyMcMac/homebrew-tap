3fd7719220b80925bfb90b0a609d0ba51d4f1918
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-08-09 17:16:38 -0400

    Update anosmellya to 0.6.4 and add configurations

--- Formula/anosmellya.rb
+++ Formula/anosmellya.rb
@@ -1,15 +1,23 @@
 class Anosmellya < Formula
   desc "Smell-based simulator of evolution through sexual reproduction"
   homepage "https://github.com/TurkeyMcMac/anosmellya"
-  url "https://github.com/TurkeyMcMac/anosmellya/archive/893677f900aa5d6c5419be87ed84e16830da7340.tar.gz"
-  version "0.5.0"
-  sha256 "2eaf54d75af01ff9fb3b6f7c4b4d43a542ed65b8f01af0a64b6a5d2c84f3a5ed"
+  url "https://github.com/TurkeyMcMac/anosmellya/archive/d57f09b133ad30270b4cf942edc52237b31bbf50.tar.gz"
+  version "0.6.4"
+  sha256 "ea05b1dd69f0b830851ed8daebebe94615abbd84d13548a22384c734c931f176"
 
   depends_on "sdl2"
 
   def install
     system "make", "CXXFLAGS=-O3 -flto"
     bin.install "anosmellya"
+    share.install "configurations"
+  end
+
+  def caveats
+    <<~EOT
+      Configuration presets are available in #{share/"configurations"}
+      Read more about each configuration preset in its respective file.
+    EOT
   end
 
   test do
