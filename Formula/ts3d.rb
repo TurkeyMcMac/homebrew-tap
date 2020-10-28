d2a1629b6f207cdcf4677f65ae90f3f1ee16b1d9
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-10-27 21:05:41 -0400

    Update ts3d to 1.9.9

--- Formula/ts3d.rb
+++ Formula/ts3d.rb
@@ -1,10 +1,10 @@
 class Ts3d < Formula
-  VERSION = "1.9.8".freeze
+  VERSION = "1.9.9".freeze
   DOWNLOAD_URL = "https://github.com/TurkeyMcMac/ts3d/archive/v#{VERSION}.tar.gz".freeze
   desc "Terminal-based first-person shooter"
   homepage "https://github.com/TurkeyMcMac/ts3d"
   url DOWNLOAD_URL
-  sha256 "bfae2fbc85301a8b74845f34b9ed0f2898fcae4e8055984c81c59699d23da563"
+  sha256 "469a7ebe9c312f94071b91b0344232d340d36472bc730381e1678e8e8e3fd506"
 
   depends_on "turkeymcmac/tap/c-test-functions" => :test
 
