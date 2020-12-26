77cf812f76f1881edb6a681d1c1d864de1567270
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-12-26 11:13:09 -0500

    Add tmv2 0.1.3 (probably not working just yet)

---
+++ Formula/tmv2.rb
@@ -0,0 +1,19 @@
+class Tmv2 < Formula
+  desc "Player for ASCII art animations that renders to the terminal"
+  homepage "https://github.com/TurkeyMcMac/tmv2"
+  url "https://github.com/TurkeyMcMac/tmv2/archive/0a10938e954279c0c0ce4147a2605c9d1e1acc31.tar.gz"
+  version "0.1.3"
+  sha256 "e39ccf78dada0efc6d712115a5f852289a2f4f1ea413fcc5359f89b525e4c988"
+
+  def install
+    system "make", "OPTIMIZATION=-O3"
+    mkdir_p bin
+    bin.install "tmv2"
+    mkdir_p man6
+    man6.install "tmv2.6.gz"
+  end
+
+  test do
+    shell_output "printf '\\na\\n\\n a\\n\\n  a' | tmv2"
+  end
+end
