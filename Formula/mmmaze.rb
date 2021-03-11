58a90b7de79d440f91b25c849bc3fadba4dfd9e6
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-03-11 17:56:47 -0500

    Update mmmaze to 0.5.10

--- Formula/mmmaze.rb
+++ Formula/mmmaze.rb
@@ -1,10 +1,10 @@
 class Mmmaze < Formula
-  VERSION = "0.5.8".freeze
+  VERSION = "0.5.10".freeze
   desc "Game about collecting money in a maze and escaping from monsters"
   homepage "https://github.com/TurkeyMcMac/mmmaze"
   url "https://github.com/TurkeyMcMac/mmmaze/archive/v#{VERSION}.tar.gz"
   version VERSION
-  sha256 "62614546306618173c0cc471c590e356a43ce34ae5209557712450777f394f75"
+  sha256 "427e1db6315b3cd3bae7b259c4aef34b26a9238dd3e0c5eec5346de2f9be8d70"
 
   def install
     system "make"
