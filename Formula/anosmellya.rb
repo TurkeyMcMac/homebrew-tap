0c34a10d59839a2e276c9fa511ac445746f3f091
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-03-28 21:57:02 -0400

    Update anosmellya to 0.9.5 and improve CXXFLAGS

--- Formula/anosmellya.rb
+++ Formula/anosmellya.rb
@@ -1,14 +1,14 @@
 class Anosmellya < Formula
   desc "Smell-based simulator of evolution through sexual reproduction"
   homepage "https://github.com/TurkeyMcMac/anosmellya"
-  url "https://github.com/TurkeyMcMac/anosmellya/archive/cce990075d15834046ff6203cd18ae3ad1249e75.tar.gz"
-  version "0.9.2"
-  sha256 "1b53c3a4f47e32f968dd64ae529c056ca1a58731121c62a920fa97d6944bb307"
+  url "https://github.com/TurkeyMcMac/anosmellya/archive/bc38bfd00851d3edc4eccc1dee6e3a93f05d0321.tar.gz"
+  version "0.9.5"
+  sha256 "fa2699bc97249abde4e70306bff1fe4243ad22d3b4540f457e1ebc1f2fb246fd"
 
   depends_on "sdl2"
 
   def install
-    system "make", "CXXFLAGS=-O3 -flto"
+    system "make", "CXXFLAGS=-O3 -flto -march=native"
     bin.install "anosmellya"
     share.install "configurations"
   end
